c03672a0d48717dcece687b9d58188158090ed22
Bryan <93453907+blopez024@users.noreply.github.com>
2025-07-21 15:50:14 -0700

    testing sign up and login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -103,7 +103,7 @@ export default function Login() {
         <LoginAlert />
         <LoginForm />
         <p className="mt-4 text-center text-sm">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link to="/signup" className="text-blue-500 hover:underline">
             Sign up
           </Link>
