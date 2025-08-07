03a33cf7805d092cd76dbcd1f4a38a20e830bd28
Bryan <93453907+blopez024@users.noreply.github.com>
2025-08-06 22:39:23 -0700

    setting up the new todo form

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -3,5 +3,76 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 
 export default function Todos() {
-  return <>HELLO</>;
+  const modalRef = useRef();
+
+  const toggleNewTodoModal = () => {
+    console.log(modalRef);
+    // Check if the modal is currently open by accessing the `open` property of `modalRef`.
+    if (modalRef.current.open) {
+      // If the modal is open, close it by calling the `close()` method.
+      modalRef.current.close();
+    } else {
+      // If the modal is not open, open it by calling the `showModal()` method.
+      modalRef.current.showModal();
+    }
+  };
+
+  function NewTodoButton() {
+    return (
+      <button className="btn btn-primary" onClick={toggleNewTodoModal}>
+        New Todo
+      </button>
+    );
+  }
+
+  function TodoModal() {
+    return (
+      <dialog ref={modalRef} className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">New Todo</h3>
+          <form>
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Name of Todo</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Description</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+            </label>
+            <div className="modal-action">
+              <button type="submit" className="btn btn-primary">
+                Create Todo
+              </button>
+              <button
+                type="button"
+                className="btn btn-ghost"
+                onClick={toggleNewTodoModal}
+              >
+                Close
+              </button>
+            </div>
+          </form>
+        </div>
+      </dialog>
+    );
+  }
+
+  return (
+    <>
+      <NewTodoButton />
+      <TodoModal />
+    </>
+  );
 }
